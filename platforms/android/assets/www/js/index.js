fe654608fb20692fe1899609b5b7e3436c4095b2
Oscar024 <or.carvajal.ortiz@gmail.com>
2017-04-21 12:35:16 -0700

    adding

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,3 +1,4 @@
+//Escanear y conectar dispositvos Bluetooth
 var dispositivos;
 document.addEventListener("deviceready", function() {
     console.log("Device is ready");
@@ -15,9 +16,35 @@ function escanear() {
 }
 
 function conectar() {
-    bluetoothSerial.connect("6B:D9:8F:C3:71:71", function() {
+    bluetoothSerial.connect("30:14:06:20:12:25", function() {
         console.log("Connected")
     }, function() {
         console.log("No se pudo conectar")
+    });
+}
+
+function desconectar() {
+    bluetoothSerial.disconnect();
+}
+
+function escribir() {
+    // Typed Array
+    var data = new Uint8Array(4);
+    data[0] = 0x41;
+    data[1] = 0x42;
+    data[2] = 0x43;
+    data[3] = 0x44;
+    bluetoothSerial.write(data, function(success) {
+        console.log(success);
+    }, function(failure) {
+        console.log(failure)
+    });
+}
+
+function leer() {
+    bluetoothSerial.read(function(data) {
+        alert(data);
+    }, function(failure) {
+        console.log(failure)
     });
 }
